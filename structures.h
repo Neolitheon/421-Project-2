0f71e376da970900be85d5ce36de16750b139e03
Michael Pattie <mjpattie@w87-167-156.wireless.uvic.ca>
2013-02-28 17:41:37 -0800

    structs were wierd so I switched to classes

---
+++ structures.h
@@ -0,0 +1,43 @@
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
+
+#include <QVector>
+
+class DataPoint
+{
+public:
+    bool classification;
+    QVector<float> attributes;
+
+    DataPoint(float attr[], int n, bool c)
+    {
+        classification = c;
+        for(int i =0;i<n;i++)
+        {
+            attributes.push_back(attr[i]);
+        }
+    }
+};
+
+class SolvedDataPoint
+{
+
+public:
+    QVector<float> attributes;
+    bool classification;
+    bool calculatedClassification;
+    bool trainingExample;
+
+    SolvedDataPoint(DataPoint d)
+    {
+        for(int i = 0; i<d.attributes.size(); i++)
+        {
+            attributes.push_back(d.attributes[i]);
+        }
+        classification = d.classification;
+        calculatedClassification = false;
+        trainingExample = false;
+    }
+};
+
+#endif // STRUCTURES_H
