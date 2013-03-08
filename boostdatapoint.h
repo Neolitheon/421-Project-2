6adec94a5d7963d32e82315693bce4fe440d49cb
Neolitheon <Nik.aleliunas@gmail.com>
2013-03-07 21:33:00 -0800

    Issues, please stand by

--- boostdatapoint.h
+++ boostdatapoint.h
@@ -7,15 +7,9 @@ class BoostDataPoint
 {
 public:
 
-<<<<<<< HEAD
     DataPoint* data;
     float weight;
     int attr_of_interest;
-=======
-    float weight;
-    int attr_of_interest;
-    DataPoint* data;
->>>>>>> 80f233be28800210c4751be4cedb19bae07c553a
 
     BoostDataPoint(DataPoint* d, float w):data(d),weight(w){}
     BoostDataPoint(){}
