35160e63f8fd4ef5e981e630e0630575b3d82baa
Neolitheon <Nik.aleliunas@gmail.com>
2013-03-15 14:36:14 -0700

    Index Errors in 33 grid fixed

--- app.py
+++ app.py
@@ -89,6 +89,7 @@ def writeToCNFFile(puzzle, min_or_ext):
 					f.write(cnf_clause)
 
 	f.write("c At most once in each 3x3 grid\n")
+	sum = 0
 	#at most once in each box
 	for z in range(1,10):
 		for i in range(0,3):
@@ -96,10 +97,11 @@ def writeToCNFFile(puzzle, min_or_ext):
 				for x in range(1,4):
 					for y in range(1,4):
 						for k in range(1+y,4):
-							print "z: %d, i: %d, j: %d, x: %d, y: %d, k: %d" % (z,i,j,x,y,k) 
+							sum += 1
 							cnf_clause = "-"+str(3*i+x)+str(3*j+y)+str(z)+" -"+str(3*i+x)+str(3*j+k)+str(z)+" 0\n"
 							f.write(cnf_clause)
-	f.write("c secondary\n")
+	print "sum : %d\n" % sum
+	sum = 0
 	for z in range(1,10):
 	    for i in range(0,3):
 	        for j in range(0,3):
@@ -107,9 +109,10 @@ def writeToCNFFile(puzzle, min_or_ext):
 	                for x in range(1,4):
 	                    for k in range(1+x,4):
 	                        for l in range(1,4):
-	                            cnf_clause = "-"+str(3*i+x)+str(3*j+y)+str(z)+" -"+str(3*i+k)+str(3*j+l)+str(z)+" 0\n"
-                                f.write(cnf_clause)  
-
+								sum += 1
+								cnf_clause = "-"+str(3*i+x)+str(3*j+y)+str(z)+" -"+str(3*i+k)+str(3*j+l)+str(z)+" 0\n"
+								f.write(cnf_clause)  
+	print "sum : %d\n" % sum
 
 	#Extended
 	if ext:
