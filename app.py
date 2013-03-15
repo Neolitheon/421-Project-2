319107c3fc86cbd1b7d65d711ee48d95620adb90
mheavenor <michael.heavenor@hotmail.com>
2013-03-15 12:34:12 -0700

    adding command line arguments

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import sys
+
 blank = ['.', '?', '*']
 
 def readFile(filename):
@@ -44,39 +46,56 @@ def initPuzzle(filename):
 		line_count += 1 
 	return puzzle
 
-def writeToCNFFile(puzzle):
+def writeToCNFFile(puzzle, min_or_ext):
+	ext = False
+	if min_or_ext == 'ext':
+		ext = True
+
 	f = open('output.cnf', 'w')
 	
-	#Minimum
-	#AT LEAST ONE NUMBER
-	for x in range (1, 10):
-		for y in range (1, 10):
-			cnf_clause = ""
-			if puzzle[x-1][y-1] == 0:
-				for z in range (1, 10):
-					cnf_clause += "%d%d%d " % (x,y,z) 
-				cnf_clause += "0\n"
-			else:
-				cnf_clause ="%d%d%d 0\n" % (x,y,puzzle[x-1][y-1]) 
-			f.write(cnf_clause)
+	if not ext:
+		#Minimum
+		#AT LEAST ONE NUMBER
+		for x in range (1, 10):
+			for y in range (1, 10):
+				cnf_clause = ""
+				if puzzle[x-1][y-1] == 0:
+					for z in range (1, 10):
+						cnf_clause += "%d%d%d " % (x,y,z) 
+					cnf_clause += "0\n"
+				else:
+					cnf_clause ="%d%d%d 0\n" % (x,y,puzzle[x-1][y-1]) 
+				f.write(cnf_clause)
+
+	if ext:
+		pass
+		#Extended
+		#AT MOST ONE NUMBER
+	#	for x from 1 to 9:
+	#		for y from 1 to 9:
+	#			for z from 1 to 8:
+	#				if m[x][y] == z
+	#					for i from z to 9:
+	#						cnf_clause += "-%d%d%d " %(x,y,i) 
+	#					cnf_clause += "0"
+
 	f.close()
-
-	#Extended
-	#AT MOST ONE NUMBER
-#	for x from 1 to 9:
-#		for y from 1 to 9:
-#			for z from 1 to 8:
-#				if m[x][y] == z
-#					for i from z to 9:
-#						cnf_clause += "-%d%d%d " %(x,y,i) 
-#					cnf_clause += "0"
-
 
 			
 def main():
-	puzzle = initPuzzle('input.txt')
+	min_or_ext_list = ['min', 'ext']
+	if len(sys.argv) != 3:
+		print("Please put in the correct arguments: <filename> <min/ext>")
+		exit(1)
+	if sys.argv[2] not in min_or_ext_list:
+		print("Please make second argument 'min' or 'ext'")
+	
+	input_file = sys.argv[1]
+	min_or_ext = sys.argv[2]
+
+	puzzle = initPuzzle(input_file)
 	printPuzzle(puzzle)
-	writeToCNFFile(puzzle)
+	writeToCNFFile(puzzle, min_or_ext)
 
 if __name__ == "__main__":
 	main()
