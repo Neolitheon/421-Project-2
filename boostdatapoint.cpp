d530401048a892821ddf2568972a94db590f28d4
Michael Pattie <mjpattie@w054-200.wireless.uvic.ca>
2013-03-21 16:30:49 -0700

    stuff

--- boostdatapoint.cpp
+++
@@ -1,5 +0,0 @@
-#include "boostdatapoint.h"
-
-/*BoostDataPoint::BoostDataPoint()
-{
-}*/
--- common.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMMON_H
-#define COMMON_H
-
-#include <QVector>
-#include <QVector>
-#include "structures.h"
-
-#endif // COMMON_H
